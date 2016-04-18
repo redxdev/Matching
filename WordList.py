bcedf031bec3d5f9e72896b837460ed135162249
Sam Bloomberg <sam@redxdev.com>
2016-04-18 09:43:03 -0400

    added to word list

--- WordList.py
+++ WordList.py
@@ -2,7 +2,11 @@ class WordList:
     def __init__(self):
         self.list = [
             ['Accurate', 'Capable or successful in reaching a target'],
-            ['Afford', 'To have enough money to pay for']
+            ['Afford', 'To have enough money to pay for'],
+            ['Alert', 'To warn (someone) of a danger, threat, or problem'],
+            ['Analyze', 'To examine in detail'],
+            ['Annual', 'Occuring once every year'],
+            ['Apparent', 'Clearly visible or understood']
         ]
 
     def getList(self):
