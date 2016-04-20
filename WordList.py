8f87cbf3e186b05e423bc7fb7918c9ddcfda6e4a
Sam Bloomberg <sam@redxdev.com>
2016-04-20 09:37:00 -0400

    more words

--- WordList.py
+++ WordList.py
@@ -6,7 +6,13 @@ class WordList:
             ['Alert', 'To warn (someone) of a danger, threat, or problem'],
             ['Analyze', 'To examine in detail'],
             ['Annual', 'Occuring once every year'],
-            ['Apparent', 'Clearly visible or understood']
+            ['Apparent', 'Clearly visible or understood'],
+            ['Assist', 'To help someone'],
+            ['Attempt', 'To make an effort to complete a task'],
+            ['Attractive', 'Pleasing or appealing to the senses'],
+            ['Awkward', 'Hard to do or deal with'],
+            ['Baggage', 'Personal belongings packed into suitcases for traveling'],
+            ['Benefit', 'An advantage gained from something']
         ]
 
     def getList(self):
