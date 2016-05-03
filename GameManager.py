d9fea1d9c590bf0e519cfb2389c970f7c02ebf97
Sam Bloomberg <sam@redxdev.com>
2016-05-03 15:58:52 -0400

    move card size and position to its own function

--- GameManager.py
+++ GameManager.py
@@ -9,13 +9,15 @@ class GameManager:
     def startGame(self, pairCount):
         self.cards = self.wordList.getRandomCards(4)
 
-    def draw(self, screen):
-        cardW, cardH = screen.get_size()
+    def generateCardPosition(self, screenSize, i):
+        cardW, cardH = screenSize
         cardW /= 9
         cardH /= 5
+        return ((i % 4) * (cardW * 2) + cardW, int(i / 4) * (cardH * 2) + cardH, cardW, cardH)
+
+    def draw(self, screen):
         for i in range(0, len(self.cards)):
             card = self.cards[i]
             if card.active:
-                x = (i % 4) * (cardW * 2) + cardW
-                y = int(i / 4) * (cardH * 2) + cardH
+                x, y, cardW, cardH = self.generateCardPosition(screen.get_size(), i)
                 card.draw(screen, (x, y), (cardW, cardH))
