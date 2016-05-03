20cf9645c0a044cd82cea0c29f54555c77d7b402
Sam Bloomberg <sam@redxdev.com>
2016-05-03 16:42:16 -0400

    show bad cards for a moment before clearing selection

--- GameManager.py
+++ GameManager.py
@@ -6,6 +6,8 @@ class GameManager:
     def __init__(self):
         self.wordList = WordList()
         self.cards = []
+        self.badCards = (None, None)
+        self.timer = 0
 
     def startGame(self, pairCount):
         self.cards = self.wordList.getRandomCards(4)
@@ -17,6 +19,15 @@ class GameManager:
         return ((i % 4) * (cardW * 2) + cardW, int(i / 4) * (cardH * 2) + cardH, cardW, cardH)
 
     def draw(self, screen):
+        bad1, bad2 = self.badCards
+        if self.timer <= 0 and bad1 is not None and bad2 is not None:
+            bad1.selected = False
+            bad2.selected = False
+            self.badCards = (None, None)
+
+        if self.timer > 0:
+            self.timer -= 0.08
+
         for i in range(0, len(self.cards)):
             card = self.cards[i]
             if card.active:
@@ -24,6 +35,10 @@ class GameManager:
                 card.draw(screen, (x, y), (cardW, cardH))
 
     def onClick(self, screen, x, y):
+        bad1, bad2 = self.badCards
+        if bad1 is not None and bad2 is not None:
+            return
+
         found = None
         for i in range(0, len(self.cards)):
             card = self.cards[i]
@@ -56,8 +71,9 @@ class GameManager:
             card.active = False
             other.active = False
         else:
-            other.selected = False
-            card.selected = False
+            card.selected = True
+            self.badCards = (card, other)
+            self.timer = 3.0
 
         for c in self.cards:
             if c.active:
